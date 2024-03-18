f240915a74f726797cc8bf3dc78d7f8d3aeb8780
Hayes Geldmacher <hayes.g850@gmail.com>
2024-03-18 11:23:21 -0600

    added gunsafe

---
+++ Assets/numButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class numButton : Interactable
+{
+
+    [serializeField] private int buttonID = 0;
+
+
+    public override void Interact()
+    {
+        base.Interact();
+        
+    }
+}
