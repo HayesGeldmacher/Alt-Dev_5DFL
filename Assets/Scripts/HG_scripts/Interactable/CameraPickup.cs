4fea246d06ca8d55eddff7432c4f800acc253d87
Hayes Geldmacher <hayes.g850@gmail.com>
2024-04-09 15:48:13 -0600

    more changes to day scene

--- Assets/Scripts/HG_scripts/Interactable/CameraPickup.cs
+++ Assets/Scripts/HG_scripts/Interactable/CameraPickup.cs
@@ -6,11 +6,31 @@ public class CameraPickup : Interactable
 {
     [SerializeField] private CameraController _camController;
     [SerializeField] private GameObject _balloon;
-    
+
+    private void Start()
+    {
+        base.Start();
+    }
+
+    private void Update()
+    {
+        base.Update();
+    }
+
     public override void Interact()
     {
         base.Interact();
         _camController.GotCamera();
         base.PickUpItem();
+        StartCoroutine(NextInteract());
+    }
+
+    private IEnumerator NextInteract()
+    {
+        yield return new WaitForSeconds(4);
+        base.Interact();
+        yield return new WaitForSeconds(3);
+        _balloon.SetActive(true);
+        base.Interact();
     }
 }
