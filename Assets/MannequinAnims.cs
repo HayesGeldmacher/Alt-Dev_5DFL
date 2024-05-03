2bdf3b7317a55f47620b94094550e40ab3ee2f2a
Hayes Geldmacher <hayes.g850@gmail.com>
2024-05-02 21:21:14 -0600

    Added more props to nighttime

---
+++ Assets/MannequinAnims.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MannequinAnims : MonoBehaviour
+{
+    [SerializeField] private Animator _anim;
+    [SerializeField] private int _animChoice;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = transform.GetComponent<Animator>();
+        _anim.SetInteger("animChoice", _animChoice);
+    }
+
+}
