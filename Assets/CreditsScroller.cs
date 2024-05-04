e9a35876e60d603e770de07e2df50a8daacf6653
Hayes Geldmacher <hayes.g850@gmail.com>
2024-05-03 22:05:31 -0600

    Credits script

---
+++ Assets/CreditsScroller.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsScroller : MonoBehaviour
+{ 
+    [SerializeField] private Transform _cam;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _stopPoint;
+    [SerializeField] private float _doubleSpeed;
+    [SerializeField] private Transform _credits;
+    private bool _scrolling = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartCredits());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_scrolling)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                _credits.Translate(Vector3.up * Time.deltaTime * _doubleSpeed);
+            }
+            else if (Input.GetMouseButton(1))
+            {
+               
+            }
+            else
+            {
+            _credits.Translate(Vector3.up * Time.deltaTime * _speed);
+
+            }
+
+            Vector3 cameraRelative = _cam.InverseTransformPoint(_credits.position);
+            Debug.Log(cameraRelative);
+
+            if(cameraRelative.y == _stopPoint)
+            {
+                _scrolling = false;
+            }
+
+        }
+    }
+
+
+    private IEnumerator StartCredits()
+    {
+        yield return new WaitForSeconds(4f);
+        Debug.Log("STARTED");
+        _scrolling = true;
+
+    }
+}
