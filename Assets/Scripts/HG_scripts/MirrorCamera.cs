68c878298798e22ca8351743486ef6552c2bcd2a
Hayes Geldmacher <hayes.g850@gmail.com>
2024-05-06 11:52:55 -0600

    Finished Mirrors

--- Assets/Scripts/HG_scripts/MirrorCamera.cs
+++ Assets/Scripts/HG_scripts/MirrorCamera.cs
@@ -9,16 +9,21 @@ public class MirrorCamera : MonoBehaviour
 
     [SerializeField] private float _minRot;
     [SerializeField] private float _maxRot;
+
+    private Vector3 _startingAngles;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+      _startingAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
+        transform.LookAt(_player, _player.rotation * Vector3.up);
+        Vector3 rotation = new Vector3(transform.localEulerAngles.x, -transform.localEulerAngles.y, -transform.localEulerAngles.z);
+       transform.localEulerAngles = rotation;
     }
 }
