3f0a374accf87773364109fc69a613771f58094c
Hayes Geldmacher <hayes.g850@gmail.com>
2024-05-14 14:57:04 -0600

    Adding to the intro tutorial

--- Assets/Scripts/HG_scripts/Interactable/Letter.cs
+++ Assets/Scripts/HG_scripts/Interactable/Letter.cs
@@ -4,126 +4,84 @@ using UnityEngine;
 
 public class Letter : Interactable
 {
-    
-    
+
+    [SerializeField] private CameraController _cam;
     [SerializeField] private Animator _letterAnim;
-    public float speed;
-    private DialogueManager _dialogueManager;
-    private Dialogue _defaultDialogue;
-    public Dialogue _phoneDialogue;
-    [SerializeField] private EvidenceManager _evidence;
-    [SerializeField] private Animator _anim;
-    private float t;
-    float duration = 4;
-    private bool _hasSpawned = false;
-    private int diaNum = 0;
-    [SerializeField] private bool _canAudio = true;
-    private bool _hasPlayed = false;
+    [SerializeField] private int _timesInteracted = 0;
+    [SerializeField] private AudioSource _interactAudio;
+    [SerializeField] private PlayerController _controller;
+    private bool _hasInteracted = false;
+    private bool _startedInteracting = false;
+    
 
     private void Start()
     {
-        //base.Start();
         base.Start();
-        _dialogueManager = GameManager.instance.GetComponent<DialogueManager>();
-        _player = PlayerController.instance.transform;
-        _defaultDialogue = base._dialogue;
     }
-
-    private void TriggerRead()
-    {
-
-    }
-
 
     private void Update()
     {
-
         base.Update();
-        if (base._startedTalking && base._isTimed)
+        if (_hasInteracted)
         {
-
-        }
-
-        if (base._canWalkAway && base._startedTalking && _player)
-        {
-            float _distance = Vector3.Distance(_player.position, transform.position);
-            if (_distance > base._dialogueDistance)
+            if (Input.GetMouseButtonDown(0))
             {
-                EndDialogue();
+                if(_timesInteracted < 4)
+                {
+                base.Interact();
+                _timesInteracted++;
+                _interactAudio.Play();
+
+                }
+                else
+                {
+                   StartCoroutine(EndInteract());
+                }
             }
 
         }
     }
 
-    //writing "virtual" in front of a function means that children scripts can add to/edit the function
     public override void Interact()
     {
-        base.currentDialogueTime = base._dialogueTimer;
-
-        if (_evidence._hasEvidence)
+        if (_hasInteracted)
         {
-            TriggerDialogue(_phoneDialogue);
-            diaNum += 1;
-
-            if (diaNum == 1)
-            {
-               
-            }
-            else if (diaNum == 2)
-            {
-                
-            }
-            else if (diaNum == 3)
-            {
-              
-            }
+          
         }
         else
         {
-
-            TriggerDialogue(_defaultDialogue);
-        }
-    }
-
-    private void TriggerDialogue(Dialogue _dialogue)
-    {
-        if (_dialogue == _phoneDialogue)
-        {
-            _evidence._hasFoundPhone = true;
-
-        }
-
-        if (!base._startedTalking)
-        {
-            _hasPlayed = false;
-            if (_dialogue == _phoneDialogue)
+            if (!_startedInteracting)
             {
-            }
-
-            Interactable _interactable = transform.GetComponent<Interactable>();
-            _dialogueManager.StartDialogue(_dialogue, _interactable);
-            base._startedTalking = true;
-        }
-        else
-        {
-            _dialogueManager.DisplayNextSentence();
-
-            if (_dialogue == _phoneDialogue && !_hasPlayed)
-            {
-
-                _anim.SetTrigger("fade");
-                _hasPlayed = true;
+                _startedInteracting = true;
+                _cam._canInteract = false;
+                _letterAnim.SetTrigger("open");
+                _controller._frozen = true;
+                StartCoroutine(StartInteract());
 
             }
 
         }
     }
 
-    public override void EndDialogue()
+    private IEnumerator StartInteract()
     {
-        _dialogueManager.EndDialogue();
-
+        yield return new WaitForSeconds(1f);
+        _startedInteracting = false;
+        _hasInteracted = true;
+        base.Interact();
+        _timesInteracted++;
     }
 
+    private IEnumerator EndInteract()
+    {
+        base.EndDialogue();
+        _hasInteracted = false;
+        _startedInteracting = false;
+        _timesInteracted = 0;
+        _letterAnim.SetTrigger("close");
+        _controller._frozen = false;
+        yield return new WaitForSeconds(1f);
+        _cam._canInteract = true;
+    }
 }
 
