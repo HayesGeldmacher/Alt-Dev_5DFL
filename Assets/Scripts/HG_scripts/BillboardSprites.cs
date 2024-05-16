ec3c0829b2b21c2798d205632602b0a9853bd55d
Hayes Geldmacher <hayes.g850@gmail.com>
2024-05-16 17:35:52 -0600

    more bullshit for the attic

--- Assets/Scripts/HG_scripts/BillboardSprites.cs
+++ Assets/Scripts/HG_scripts/BillboardSprites.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class BillboardSprites : MonoBehaviour
 {
+
+    [SerializeField] private Transform _sprite;
+    private Transform _player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(_sprite == null)
+        {
+            _sprite = transform;
+        }
+
+        _player = PlayerController.instance.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(Camera.main.transform.position, Vector3.up);
     }
 }
