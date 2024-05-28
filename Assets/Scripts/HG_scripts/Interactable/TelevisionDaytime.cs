ebae02421a676623530b131f165eee2ec886eb2c
Hayes Geldmacher <hayes.g850@gmail.com>
2024-05-28 12:33:43 -0600

    Updating TV sequence

--- Assets/Scripts/HG_scripts/Interactable/TelevisionDaytime.cs
+++ Assets/Scripts/HG_scripts/Interactable/TelevisionDaytime.cs
@@ -10,31 +10,58 @@ public class TelevisionDaytime : Interactable
     [SerializeField] private bool _canMoveOn = false;
     [SerializeField] private bool _canExit = false;
     [SerializeField] private bool _completed = true;
-
+    [SerializeField] private PlayerController _controller;
+    [SerializeField] private CameraController _camController;
+    [SerializeField] Interactable _tv;
+    [SerializeField] private AudioSource _staticIntro;
+    [SerializeField] private AudioSource _staticContinue;
 
 
     public override void Interact()
     {
+      
+        
         if (!_hasInteracted)
         {
+            _staticIntro.Play();
             _hasInteracted = true;
             _isInteracting = true;
+            _controller._frozen = true;
+            _camController._frozen = true;
             StartCoroutine(StartSequence());
         }
-        else if (_isInteracting) 
-        {
-            if (_canExit)
-            {
-                StartCoroutine(Disappear());
-            }
-            else if (_canMoveOn)
-            {
-                _canMoveOn = false;
-                StartCoroutine(FaceAppear());
-            }
+        
+    }
 
+   private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && _hasInteracted && !_completed)
+        {
+          
+            if (_isInteracting)
+            {
+                if (_canExit)
+                {
+                    StartCoroutine(Disappear());
+                }
+                else if (_canMoveOn)
+                {
+                    _canMoveOn = false;
+                    StartCoroutine(FaceAppear());
+                }
+
+            }
+        }
+
+        if (!_staticIntro.isPlaying && _hasInteracted)
+        {
+            if (!_staticContinue.isPlaying)
+            {
+                _staticContinue.Play();
+            }
         }
     }
+
 
     private IEnumerator StartSequence()
     {
@@ -58,6 +85,15 @@ public class TelevisionDaytime : Interactable
     {
         yield return new WaitForSeconds(0.1f);
         _anim.SetTrigger("disappear");
+        yield return new WaitForSeconds(1);
+        _controller._frozen = false;
+        _camController._frozen = false;
         _completed = true;
+        _tv.enabled = true;
+        _staticContinue.Stop();
+        _staticIntro.Stop();
+        TelevisionDaytime _this = this;
+        base.EndDialogue();
+        Destroy(_this);
     }
 }
