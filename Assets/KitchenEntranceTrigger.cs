8cc83f55f1a9ca1b3843fc63eee18ab8ad78de3b
Hayes Geldmacher <hayes.g850@gmail.com>
2024-08-22 23:05:18 -0600

    fucking Me UP

---
+++ Assets/KitchenEntranceTrigger.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenEntranceTrigger : MonoBehaviour
+{
+    private bool _canTrigger = true;
+    [SerializeField] private List<GameObject> _disappearItems = new List<GameObject>();
+    [SerializeField] private List<GameObject> _appearItems = new List<GameObject>();
+
+    [SerializeField] private Animator _light1;
+
+    private void Start()
+    {
+        _light1.SetTrigger("on");
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (_canTrigger)
+            {
+                _canTrigger = false;
+            
+            }
+        }
+    }
+
+    private IEnumerator TransitionLevel()
+    {
+        _light1.SetTrigger("fade");
+        yield return new WaitForSeconds(1f);
+        foreach (GameObject _obj in _disappearItems)
+        {
+            _obj.SetActive(false);
+        }
+        foreach (GameObject _obj in _appearItems)
+        {
+            _obj.SetActive(true);
+        }
+
+    }
+}
