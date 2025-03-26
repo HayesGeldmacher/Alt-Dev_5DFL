68f3a3d19f285268868d568cad7ac058b7dc8ce8
Hayes Geldmacher <42843199+HayesGeldmacher@users.noreply.github.com>
2025-03-26 16:46:19 -0600

    some final freaking changes motherfucker!

--- Assets/BoilerAudioNear.cs
+++ Assets/BoilerAudioNear.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class BoilerAudioNear : MonoBehaviour
 {
@@ -9,17 +10,20 @@ public class BoilerAudioNear : MonoBehaviour
     [SerializeField] private LayerMask _playerMask;
 
     [SerializeField] private AudioSource _audio;
+    [SerializeField] private float _currentAudio;
+    [SerializeField] private float _audioDecreaseRate;
+    [SerializeField] private bool _inSight;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        _currentAudio = _audio.volume;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        _currentAudio = _audio.volume;
         Vector3 direction = _playerBody.position - transform.position;
 
         RaycastHit hit;
@@ -27,25 +31,43 @@ public class BoilerAudioNear : MonoBehaviour
         {
             if (hit.transform.tag == "Player")
             {
-                if (!_audio.isPlaying)
-                {
-                    _audio.Play();
-                }
+                _inSight = true;
+                IncreaseUpdate();
             }
             else
             {
-                if (_audio.isPlaying)
-                {
-                    _audio.Pause();
-                }
+                _inSight = false;
+                DecreaseUpdate();
             }
         }
         else
         {
-            if (_audio.isPlaying)
-            {
-                _audio.Pause();
-            }
+            _inSight = false;
+            DecreaseUpdate();
         }
     }
+
+    private void DecreaseUpdate()
+    {
+        float currentVol = _currentAudio;
+        if(_currentAudio > 0f)
+        {
+            currentVol -= (1 * _audioDecreaseRate * Time.deltaTime);
+        }
+
+        _audio.volume = currentVol;
+    }
+
+    private void IncreaseUpdate()
+    {
+        float currentVol = _currentAudio;
+        if (_currentAudio < 1)
+        {
+            currentVol += (1 * _audioDecreaseRate * Time.deltaTime);
+        }
+
+        _audio.volume = currentVol;
+    }
+
+
 }
