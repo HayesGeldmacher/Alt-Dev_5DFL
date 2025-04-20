1f31349081b1029739e517427629a4f72bf73651
Hayes Geldmacher <hayes.g850@gmail.com>
2025-04-20 02:13:47 -0600

    Fixed a bunhc of freaking bugs!

--- Assets/Scripts/HG_scripts/MirrorCamera.cs
+++ Assets/Scripts/HG_scripts/MirrorCamera.cs
@@ -7,8 +7,10 @@ public class MirrorCamera : MonoBehaviour
     [SerializeField] private Transform _camera;
     [SerializeField] private Transform _player;
 
-    [SerializeField] private float _minRot;
-    [SerializeField] private float _maxRot;
+    [SerializeField] private float _minRotX;
+    [SerializeField] private float _maxRotX;
+
+    [SerializeField] private bool _limitRotation = false;
 
     private Vector3 _startingAngles;
     
@@ -24,6 +26,12 @@ public class MirrorCamera : MonoBehaviour
       
         transform.LookAt(_player, _player.rotation * Vector3.up);
         Vector3 rotation = new Vector3(transform.localEulerAngles.x, -transform.localEulerAngles.y, -transform.localEulerAngles.z);
+
+        if (_limitRotation)
+        {
+            rotation = new Vector3(Mathf.Clamp(rotation.x, _minRotX, _maxRotX), rotation.y, rotation.z);
+        }
+
        transform.localEulerAngles = rotation;
     }
 }
