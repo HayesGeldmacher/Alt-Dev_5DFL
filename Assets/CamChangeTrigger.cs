de44176a389b6bcc12f45743d8df9ff225e50acf
Hayes Geldmacher <42843199+HayesGeldmacher@users.noreply.github.com>
2025-05-06 16:06:15 -0600

    went up

--- Assets/CamChangeTrigger.cs
+++ Assets/CamChangeTrigger.cs
@@ -6,15 +6,45 @@ public class CamChangeTrigger : MonoBehaviour
 {
 
 
+    [SerializeField] private CamChangePositions _camChange;
+    [SerializeField] private int _camNumForward;
+    [SerializeField] private int _camNumBack;
+    private bool _forward = true;
+    private bool _canEnter = true;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
+        if (_canEnter)
+        {
+            if(other.tag == "Player")
+            {
+                if (_forward) 
+                {
+                    _camChange.ChangePos(_camNumForward);
+                }
+                else
+                {
+                    _camChange.ChangePos(_camNumBack);
+                }
+
+                _forward = !_forward;
+                _canEnter = false;
+            }
+        }
         
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            _canEnter = true;
+        }
+    }
+
 }
