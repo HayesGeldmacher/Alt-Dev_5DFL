d5461d44d0dcff4ad8afffbe66b9387eda75ba4b
Hayes Geldmacher <42843199+HayesGeldmacher@users.noreply.github.com>
2025-06-16 12:22:37 -0600

    Working lots more on night segment

--- Assets/BedNight.cs
+++ Assets/BedNight.cs
@@ -60,25 +60,25 @@ public class BedNight : Interactable
     public override void Interact()
     {
 
+           _startedEnd = true;
+          StartCoroutine(CompleteLevel());
         if (!_startedEnd)
         {
             if (!_door._isOpen)
             {
                 if (_watch._activated)
                 {
-                     _startedEnd = true;
-                    StartCoroutine(CompleteLevel());
                 }
                 else
                 {
-                    base._dialogue._sentences[0] = "Watching something on my computer might help me sleep...";
-                    base.Interact();
+                   // base._dialogue._sentences[0] = "Watching something on my computer might help me sleep...";
+                    //base.Interact();
                 }
             }
             else
             {
-                base._dialogue._sentences[0] = "I can't go to sleep with the door open...";
-                base.Interact();
+              //  base._dialogue._sentences[0] = "I can't go to sleep with the door open...";
+              //  base.Interact();
             }
         }
 
