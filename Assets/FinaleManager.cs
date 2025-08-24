c113aa158f3c4714d31cbc5aea791b2e540b4f81
Hayes Geldmacher <42843199+HayesGeldmacher@users.noreply.github.com>
2025-08-23 21:43:44 -0600

    Worked bunch more finale

--- Assets/FinaleManager.cs
+++ Assets/FinaleManager.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Cinemachine;
+using UnityEngine.SceneManagement; 
 public class FinaleManager : MonoBehaviour
 {
     [Header("Scene Timing Fields")]
@@ -10,7 +11,6 @@ public class FinaleManager : MonoBehaviour
     public float _closeTime; //how long after start fading to end game, go to credits
 
     public Animator _eyesAnim; //animator for eyes slowly close and fade away
-    public AudioSource _ambientAudio; //ambience, people talking, et cetera
     //add something here for camera can move limited!
     
 
@@ -18,7 +18,28 @@ public class FinaleManager : MonoBehaviour
     private bool _ended = false;
 
     private Interactable _dialogueInteract;
-    
+
+    public CRT _crt;
+
+
+    [Header("Camera Zoom")]
+    [SerializeField] private CinemachineVirtualCamera _virtualCam;
+    [SerializeField] private float _endCamZoom;
+    [SerializeField] private float _camZoomSpeed;
+    [SerializeField] private bool _zooming = false;
+    [SerializeField] private float _currentZoom;
+    [SerializeField] private Transform _cam;
+    [SerializeField] private Transform _camMovePos;
+    [SerializeField] private float _camFloatSpeed;
+
+
+
+    [Header("Audio Fade")]
+    [SerializeField] private AudioSource _ambientAudio;
+    [SerializeField] private float _fadeSpeed;
+
+    [SerializeField] private bool _fadingAudio = false;
+
     //scene lasts for 60 seconds, and then slowly fades out!
     // Start is called before the first frame update
     void Start()
@@ -33,6 +54,9 @@ public class FinaleManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
+
         if (_started)
         {
             if (!_ended)
@@ -41,29 +65,74 @@ public class FinaleManager : MonoBehaviour
                 if (_openTime <= 0)
                 {
                     _ended = true;
-                    StartCoroutine(EndScene());
+                    StartCoroutine(StartZoom());
                 }
             }
+        }
+
+
+        if (_zooming)
+        {
+            _currentZoom = _virtualCam.m_Lens.FieldOfView;
+            float newZoom = _currentZoom;
+            newZoom -= (Time.deltaTime * _camZoomSpeed);
+
+            
+
+           if(_currentZoom <= _endCamZoom)
+            {
+                _zooming = false;
+            }
+
+            _virtualCam.m_Lens.FieldOfView = newZoom;
+
+
+
+            //moving the camera!
+            Vector3 currentPos = _cam.localPosition;
+            Vector3 newPos = Vector3.Lerp(currentPos, _camMovePos.localPosition, _camFloatSpeed * Time.deltaTime);
+            _cam.localPosition = newPos;
+        }
+
+        if (_fadingAudio)
+        {
+            float currentVolume = _ambientAudio.volume;
+            currentVolume -= (_fadeSpeed * Time.deltaTime);
+            _ambientAudio.volume = currentVolume;
         }
     }
 
     private IEnumerator BeginScene()
     {
+        _crt.StartBreathing();
         yield return new WaitForSeconds(_introWait);
         _eyesAnim.SetTrigger("fade");
         StartAudio();
         _started = true;
+        yield return new WaitForSeconds(2f);
+        _crt.EndBreathing();
         //allow cam to move limited
     }
 
+
+    private IEnumerator StartZoom()
+    {
+        _zooming = true;
+        yield return new WaitForSeconds(35f);
+        _eyesAnim.SetTrigger("veryLong");
+        _fadingAudio = true;
+        yield return new WaitForSeconds(10f);
+        StartCoroutine(EndScene());
+
+    }
+
+
+
     private IEnumerator EndScene()
     {
-        //stop cam from moving
-        EndAudio();
-        _eyesAnim.SetTrigger("close");
-        yield return new WaitForSeconds(_closeTime);
-        //end game, go to credits!1
-        GameManager.instance.LoadSceneSpecific("Credits");
+
+      yield return new WaitForSeconds(0.1f);
+        SceneManager.LoadScene("Credits");
 
     }
 
