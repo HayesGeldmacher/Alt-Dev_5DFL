5f18677f32f86b7496ca194e662166f41aa17402
Hayes Geldmacher <42843199+HayesGeldmacher@users.noreply.github.com>
2026-01-17 22:30:43 -0700

    Working on load scenee - HG

--- Assets/TitleScreenManager.cs
+++ Assets/TitleScreenManager.cs
@@ -13,24 +13,105 @@ public class TitleScreenManager : MonoBehaviour
     [SerializeField] TitleScreenSpriteFollowMouse _spriteFollow;
 
     [SerializeField] private Datamosh _data;
+
+
+    [Header("Audio Fields")]
+    public bool fadeAudio = false;
+    [SerializeField] private AudioSource audioToFade;
+    [SerializeField] private float fadeSpeed;
+    [SerializeField] private float fadeSpeedDown;
+    [SerializeField] private float minFadeUp = 0.4f;
+    private bool isFading = false;
+    private bool isFadingUp = false;
+   
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Confined;
         _spriteFollow.EnableCursor(true);
+
+        if (fadeAudio)
+        {
+            isFadingUp = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        if (isFading)
+        {
+            
+            float oldVolume = audioToFade.volume;
+
+            if (oldVolume <= 0)
+            {
+                isFading = false;
+                return;
+            }
+
+            float newVolume = oldVolume - (fadeSpeedDown * Time.deltaTime);
+            audioToFade.volume = newVolume;
+
+        }
+
+        if (isFadingUp)
+        {
+            float oldVolume = audioToFade.volume;
+
+            if (oldVolume >= minFadeUp)
+            {
+                isFadingUp = false;
+                return;
+            }
+
+            float newVolume = oldVolume + (fadeSpeed * Time.deltaTime);
+            audioToFade.volume = newVolume;
+        }
+    }
+
+    public void CallReturnMainMenu()
+    {
+        if (!_startedTransition)
+        {
+            if (fadeAudio)
+            {
+                isFading = true;
+                isFadingUp = false;
+            }
+            
+            _startedTransition = true;
+            _interactAudio.Play();
+            StartCoroutine(ReturnMenu());
+        }
+    }
+
+    private IEnumerator ReturnMenu()
+    {
+        CallDataGlitch();
+        _blackOutAnim.SetTrigger("fade");
+        Cursor.visible = false;
+        _cursorSprite.SetParent(null);
+        _cursorSprite.GetComponent<Animator>().SetTrigger("fade");
+        Cursor.lockState = CursorLockMode.Locked;
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("TitleScreen");
     }
 
     public void CallStartGame()
     {
+
+
         if (!_startedTransition)
         {
+            if (fadeAudio)
+            {
+                isFading = true;
+                isFadingUp = false;
+            }
+
             _startedTransition = true;
             _interactAudio.Play();
             StartCoroutine(StartGame());
@@ -46,14 +127,21 @@ public class TitleScreenManager : MonoBehaviour
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         yield return new WaitForSeconds(1);
-        //SceneManager.LoadScene("Prologue");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Prologue");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void CallExitGame()
     {
+
+
         if (!_startedTransition)
         {
+            if (fadeAudio)
+            {
+                isFading = true;
+                isFadingUp = false;
+            }
             _startedTransition = true;
             _interactAudio.Play();
             StartCoroutine(ExitGame());
@@ -76,5 +164,35 @@ public class TitleScreenManager : MonoBehaviour
     {
         _data.Glitch();
         _data.Glitch();
+    }
+
+
+    public void CallLoadDay(string day)
+    {
+
+
+        if (!_startedTransition)
+        {
+            if (fadeAudio)
+            {
+                isFading = true;
+                isFadingUp = false;
+            }
+            _startedTransition = true;
+            _interactAudio.Play();
+            StartCoroutine(LoadDay(day));
+        }
+    }
+
+    private IEnumerator LoadDay(string day)
+    {
+        CallDataGlitch();
+        _blackOutAnim.SetTrigger("fade");
+        Cursor.visible = false;
+        _cursorSprite.SetParent(null);
+        _cursorSprite.GetComponent<Animator>().SetTrigger("fade");
+        Cursor.lockState = CursorLockMode.Locked;
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(day);
     }
 }
